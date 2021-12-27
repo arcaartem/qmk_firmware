cc264aac9a6123d61594a0374715e239e8fb66d0
Arca <arca.artem@gmail.com>
2021-12-27 13:10:44 +0000

    update crkbd to latest

--- keyboards/crkbd/keymaps/red-nomad/rules.mk
+++ keyboards/crkbd/keymaps/red-nomad/rules.mk
@@ -1,6 +1,7 @@
 RGB_MATRIX_ENABLE   = yes
 VIA_ENABLE          = no      # Enable VIA
-OLED_DRIVER_ENABLE  = yes
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306
 TAP_DANCE_ENABLE    = yes
 RAW_ENABLE          = yes
 DEBOUNCE_TYPE       = sym_defer_pk
