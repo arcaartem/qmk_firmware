ea938bab483a9c6d6eb33934abba89fc8553b7ea
Arca <arca.artem@gmail.com>
2022-07-02 23:16:50 +0100

    move tap dance code to own file

---
+++ keyboards/crkbd/keymaps/red-nomad/tap_dance_helper.h
@@ -0,0 +1,41 @@
+#ifndef TAP_DANCE_HELPER__H
+#define TAP_DANCE_HELPER__H
+
+typedef enum {
+    TD_NONE,
+    TD_UNKNOWN,
+    TD_SINGLE_TAP,
+    TD_SINGLE_HOLD,
+    TD_DOUBLE_TAP,
+    TD_DOUBLE_HOLD,
+    TD_DOUBLE_SINGLE_TAP, // Send two single taps
+    TD_TRIPLE_TAP,
+    TD_TRIPLE_HOLD
+} td_state_t;
+
+typedef struct {
+    bool is_press_action;
+    td_state_t state;
+} td_tap_t;
+
+enum {
+    TD_LSFT_HYPR,
+    TD_LALT_MEH,
+    TD_LALT_RALT,
+    TD_RALT_MEH,
+    TD_RALT_GUI,
+    TD_LAYER_QUOTE,
+    TD_LAYER_SLASH,
+    TD_LAYER_MEH,
+};
+
+td_state_t cur_dance(qk_tap_dance_state_t *state);
+
+void layer_quote_dance_finished(qk_tap_dance_state_t *state, void *user_data);
+void layer_quote_dance_reset(qk_tap_dance_state_t *state, void *user_data);
+void layer_slash_dance_finished(qk_tap_dance_state_t *state, void *user_data);
+void layer_slash_dance_reset(qk_tap_dance_state_t *state, void *user_data);
+void layer_meh_dance_finished(qk_tap_dance_state_t *state, void *user_data);
+void layer_meh_dance_reset(qk_tap_dance_state_t *state, void *user_data);
+
+#endif
