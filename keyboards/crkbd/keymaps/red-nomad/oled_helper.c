6e6dce7a9d48fda517ac7665897be4e5ca097b42
Arca <arca.artem@gmail.com>
2022-07-02 23:49:22 +0100

    extract oled functions to own file

---
+++ keyboards/crkbd/keymaps/red-nomad/oled_helper.c
@@ -0,0 +1,188 @@
+#include QMK_KEYBOARD_H
+#include "layers.h"
+
+#ifdef OLED_ENABLE
+
+#define OLED_USER_TIMEOUT 30000
+
+uint8_t hid_buffers[2][32] = { 0 };
+uint8_t hid_buffer_lengths[2] = { 0 };
+
+oled_rotation_t oled_init_user(oled_rotation_t rotation) {
+  if (is_keyboard_master()) {
+    return OLED_ROTATION_0;
+  }
+  return OLED_ROTATION_180;
+}
+
+void raw_hid_receive(uint8_t *data, uint8_t length) {
+    if (is_oled_on()) {
+        uint8_t buffer_select = data[0];
+        uint8_t data_length = data[1];
+        dprintf("raw_hid_receive - buffer_select: %d, data_lenght: %d, length: %d\n", buffer_select, data_length, length);
+        if (buffer_select > 1 || data_length > 29) {
+            return;
+        }
+
+        hid_buffer_lengths[buffer_select] = data_length;
+        for (uint8_t i=0; i<data_length && i<30; i++) {
+            hid_buffers[buffer_select][i] = data[i+2];
+        }
+    }
+}
+
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("L:"), false);
+    switch (layer_state) {
+        case L_BASE:
+            switch (default_layer_state) {
+                case L_BASE:
+                case L_BASE1:
+                    oled_write_ln_P(PSTR("Base 1"), false);
+                    break;
+                case L_BASE2:
+                    oled_write_ln_P(PSTR("Base 2"), false);
+                    break;
+                default:
+                    oled_write_ln_P(PSTR("------"), false);
+            }
+            break;
+        case L_LOWER:
+            oled_write_ln_P(PSTR("Lower "), false);
+            break;
+        case L_RAISE:
+            oled_write_ln_P(PSTR("Raise "), false);
+            break;
+        case L_ADJUST:
+        case L_ADJUST|L_LOWER:
+        case L_ADJUST|L_RAISE:
+        case L_ADJUST|L_LOWER|L_RAISE:
+            oled_write_ln_P(PSTR("Device"), false);
+            break;
+        case L_FUNCTION:
+            oled_write_ln_P(PSTR("F Keys"), false);
+            break;
+    }
+}
+
+void oled_print_mod(uint16_t mods, uint16_t mod_mask, char * indicator) {
+    if ((mods & mod_mask) > 0)
+        oled_write(indicator, false);
+    else
+        oled_write_P(PSTR(" "), false);
+}
+
+void oled_render_mod_state(void) {
+    oled_write_P(PSTR("M:"), false);
+    uint16_t mods = get_mods();
+    oled_write_P(((mods & MOD_MASK_GUI) > 0) ? PSTR("G") : PSTR("_"), false);
+    oled_write_P(((mods & MOD_MASK_CTRL) > 0) ? PSTR("C") : PSTR("_"), false);
+    oled_write_P(((mods & MOD_MASK_ALT) > 0) ? PSTR("A") : PSTR("_"), false);
+    oled_write_P(((mods & MOD_MASK_SHIFT) > 0) ? PSTR("S") : PSTR("_"), false);
+    oled_write_P(PSTR(" "), false);
+}
+
+void oled_render_host_led_status(void) {
+    oled_write_P(PSTR("H:"), false);
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("N") : PSTR("_"), false);
+    oled_write_P(led_state.caps_lock ? PSTR("C") : PSTR("_"), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("S") : PSTR("_"), false);
+    oled_write_P(PSTR(" "), false);
+}
+
+void oled_render_layer_mod_host_state(void) {
+    oled_render_mod_state();
+    oled_render_host_led_status();
+    oled_render_layer_state();
+}
+
+char keylog_str[24] = {};
+
+const char code_to_name[60] = {
+    ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
+    'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
+    '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
+
+void set_keylog(uint16_t keycode, keyrecord_t *record) {
+  char name = ' ';
+  uint16_t rawKeycode = keycode;
+    if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
+        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
+  if (keycode < 60) {
+    name = code_to_name[keycode];
+  }
+
+  // update keylog
+  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k[%04x]:'%c'",
+           record->event.key.row, record->event.key.col,
+           rawKeycode, name);
+}
+
+void oled_render_keylog(void) {
+    oled_write(keylog_str, false);
+}
+
+void oled_render_hid_buffer(uint8_t buffer) {
+    uint8_t length = hid_buffer_lengths[buffer];
+    /* dprintf("oled_render_hid_buffer - buffer: %d, length: %d\n", buffer, length); */
+    if (length > 0) {
+        for (uint8_t i=0; i<length; i++) {
+            oled_write_char((char)hid_buffers[buffer][i], false);
+        }
+    }
+}
+
+void oled_render_hid_message(void) {
+    oled_render_hid_buffer(0);
+    oled_write_ln_P(PSTR(""), false);
+    oled_render_hid_buffer(1);
+}
+
+void oled_render_logo(void) {
+    static const char PROGMEM crkbd_logo[] = {
+        0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
+        0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
+        0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
+        0};
+    oled_write_P(crkbd_logo, false);
+}
+
+bool oled_task_user(void) {
+#ifdef ENCODER_ENABLE
+    bool is_activity_elapsed = (last_matrix_activity_elapsed() > OLED_USER_TIMEOUT && last_encoder_activity_elapsed() > OLED_USER_TIMEOUT);
+#else
+    bool is_activity_elapsed = (last_matrix_activity_elapsed() > OLED_USER_TIMEOUT);
+#endif
+
+    if (is_activity_elapsed) {
+        if (is_oled_on()) {
+            oled_off();
+        }
+        return false;
+    } else if (!is_oled_on()) {
+        oled_on();
+    }
+
+#
+    if (is_keyboard_master()) {
+        oled_render_layer_mod_host_state();
+        oled_render_keylog();
+        oled_render_hid_message();
+    } else {
+        oled_render_logo();
+    }
+    return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (record->event.pressed) {
+    set_keylog(keycode, record);
+  }
+  return true;
+}
+
+#endif // OLED_ENABLE
