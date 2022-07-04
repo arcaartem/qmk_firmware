f9b5c42b4853cde3592e9df25a48ebd2b56bf58a
Arca <arca.artem@gmail.com>
2022-07-04 11:36:49 +0100

    fix layers

--- keyboards/crkbd/keymaps/red-nomad/layers.h
+++ keyboards/crkbd/keymaps/red-nomad/layers.h
@@ -2,18 +2,19 @@
 #define LAYERS__H
 
 #define BASE 0
-#define BASE2 1
+#define BASE_ALT 1
 #define L1 2
-#define L2 3
-#define L3 4
-#define L4 5
+#define L1_ALT 3
+#define L2 4
+#define L3 5
+#define L4 6
 
 #define L_BASE 0
-#define L_BASE1 1
-#define L_BASE2 2
+#define L_BASE_ALT 2
 #define L_LOWER 4
-#define L_RAISE 8
-#define L_ADJUST 16
-#define L_FUNCTION 32
+#define L_LOWER_ALT 8
+#define L_RAISE 16
+#define L_ADJUST 32
+#define L_FUNCTION 64
 
 #endif
