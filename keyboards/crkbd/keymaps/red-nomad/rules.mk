78389bdefa7a76ea18d3d05503e9f3a40e6524e0
Arca <arca.artem@gmail.com>
2022-07-09 12:29:29 +0100

    remove unnecessary tap dance functions

--- keyboards/crkbd/keymaps/red-nomad/rules.mk
+++ keyboards/crkbd/keymaps/red-nomad/rules.mk
@@ -1,6 +1,6 @@
 RGBLIGHT_ENABLE              =  no
 TAP_DANCE_ENABLE             =  yes
-RAW_ENABLE                   =  yes
+RAW_ENABLE                   =  no
 DEBOUNCE_TYPE                =  sym_defer_pk
 CONSOLE_ENABLE               =  no
 DYNAMIC_TAPPING_TERM_ENABLE  =  no
