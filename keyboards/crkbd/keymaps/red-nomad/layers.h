a0ccbf01958d5a3ea5c0a1e029cd8696b16adab7
Arca <arca.artem@gmail.com>
2023-09-23 22:56:41 +0100

    Export keymap from old branch 'red-nomad-old'

---
+++ keyboards/crkbd/keymaps/red-nomad/layers.h
@@ -0,0 +1,20 @@
+#ifndef LAYERS__H
+#define LAYERS__H
+
+#define BASE 0
+#define BASE_ALT 1
+#define L1 2
+#define L2 3
+#define L3 4
+#define L4 5
+#define L5 6
+
+#define L_BASE 1
+#define L_BASE_ALT 2
+#define L_LOWER 4
+#define L_RAISE 8
+#define L_MOUSE 16
+#define L_MEDIA 32
+#define L_FUNCTION 64
+
+#endif
---
+++ keyboards/crkbd/keymaps/red-nomad/rules.mk
@@ -0,0 +1,13 @@
+OLED_ENABLE                  =  yes
+RGBLIGHT_ENABLE              =  no
+TAP_DANCE_ENABLE             =  yes
+RAW_ENABLE                   =  no
+DEBOUNCE_TYPE                =  sym_defer_pk
+CONSOLE_ENABLE               =  no
+DYNAMIC_TAPPING_TERM_ENABLE  =  no
+RGB_MATRIX_ENABLE = yes
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+
+SRC += tap_dance_helper.c oled_helper.c
