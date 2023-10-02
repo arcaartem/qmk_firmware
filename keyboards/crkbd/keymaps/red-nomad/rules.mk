b641396082862e9fc9d9b6153db32daa0340103b
Arca <arca.artem@gmail.com>
2023-10-02 23:11:07 +0100

    Enable dynamic macros

--- keyboards/crkbd/keymaps/red-nomad/rules.mk
+++ keyboards/crkbd/keymaps/red-nomad/rules.mk
@@ -5,6 +5,7 @@ RAW_ENABLE                   =  no
 DEBOUNCE_TYPE                =  sym_defer_pk
 CONSOLE_ENABLE               =  no
 DYNAMIC_TAPPING_TERM_ENABLE  =  no
+DYNAMIC_MACRO_ENABLE = yes
 RGB_MATRIX_ENABLE = yes
 
 SPACE_CADET_ENABLE = no
