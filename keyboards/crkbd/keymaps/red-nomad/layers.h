ce1115ad7a65db3153df891c4dade848526816fd
Arca <arca.artem@gmail.com>
2023-11-02 06:44:41 +0000

    Add another base layer with arrow keys

--- keyboards/crkbd/keymaps/red-nomad/layers.h
+++ keyboards/crkbd/keymaps/red-nomad/layers.h
@@ -3,18 +3,20 @@
 
 #define BASE 0
 #define BASE_ALT 1
-#define L1 2
-#define L2 3
-#define L3 4
-#define L4 5
-#define L5 6
+#define BASE_ALT2 2
+#define L1 3
+#define L2 4
+#define L3 5
+#define L4 6
+#define L5 7
 
 #define L_BASE 1
 #define L_BASE_ALT 2
-#define L_LOWER 4
-#define L_RAISE 8
-#define L_MOUSE 16
-#define L_MEDIA 32
-#define L_FUNCTION 64
+#define L_BASE_ALT2 4
+#define L_LOWER 8
+#define L_RAISE 16
+#define L_MOUSE 32
+#define L_MEDIA 64
+#define L_FUNCTION 128
 
 #endif
